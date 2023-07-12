d79f34ae4f030c0d72a83795a7f021f4777cebe9
yumixx1 <133470969+yumixx1@users.noreply.github.com>
2023-07-12 09:18:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -43,6 +43,27 @@ function calculateMBTI() {
   const mbtiResult = `${eCount > iCount ? 'E' : 'I'}${sCount > nCount ? 'S' : 'N'}${tCount > fCount ? 'T' : 'F'}${jCount > pCount ? 'J' : 'P'}`;
   const resultElement = document.getElementById('result');
   resultElement.innerHTML = `당신의 MBTI 결과: ${mbtiResult}`;
+
+  const personalityType = getPersonalityType(mbtiResult);
+  const personalityElement = document.getElementById('personality');
+  personalityElement.innerHTML = `당신의 성격 유형: ${personalityType}`;
+}
+
+function getPersonalityType(mbtiResult) {
+  // 각 MBTI 유형에 따라 성격 유형을 반환하는 함수
+  // 여기에서는 예시로 몇 가지 유형만 포함되어 있습니다.
+  switch (mbtiResult) {
+    case 'ISTJ':
+      return '현실적인 관리자';
+    case 'ENFP':
+      return '재기발랄한 영감주의자';
+    case 'ISTP':
+      return '논리적인 공영주의자';
+    case 'ESFJ':
+      return '사교적인 친선도모자';
+    default:
+      return '알 수 없음';
+  }
 }
 
 const resultButton = document.querySelector('input[type="button"]');
